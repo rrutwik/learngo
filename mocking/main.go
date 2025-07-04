67b105b80106eb8e18675a40c03035203938a3bd
Rutwik <rutwikpatel08@gmail.com>
2025-07-04 14:14:44 +0530

    newsleeper

--- mocking/main.go
+++ mocking/main.go
@@ -7,6 +7,23 @@ import (
 	"time"
 )
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 type Sleeper interface {
 	Sleep()
 }
@@ -18,8 +35,10 @@ func (d *DefaultSleeper) Sleep() {
 }
 
 func main() {
-	sleeper := DefaultSleeper{}
-	Countdown(os.Stdout, &sleeper)
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+	// sleeper := DefaultSleeper{}
+	// Countdown(os.Stdout, &sleeper)
 }
 
 const finalWord = "Go!"
