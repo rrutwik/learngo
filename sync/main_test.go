27808635dd3da35d2b8212149adcdaff37457268
Rutwik <rutwikpatel08@gmail.com>
2025-07-04 16:39:31 +0530

    sync

---
+++ sync/main_test.go
@@ -0,0 +1,37 @@
+package sync
+
+import (
+	"sync"
+	"testing"
+)
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
+
+func TestCounter(t *testing.T) {
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+}
